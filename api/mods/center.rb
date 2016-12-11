b585eaad299a6846edae2364576ac3f2094e6656
Saravana <sgsaravana@gmail.com>
2016-12-12 01:22:10 +0800

    test done for edit center and edit center address

--- api/mods/center.rb
+++ api/mods/center.rb
@@ -1,16 +1,29 @@
 module Venuebook
 
-   class CenterAPI < Grape::API
-      namespace "center" do
-	 post do
-	    Center.create(params)
-	 end
+	class CenterAPI < Grape::API
+		namespace "center" do
+			post do
+				Center.create(params)
+			end
 
-	 post "/:id/address" do
-	    center = Center.find(id: params[:id])
-	    center.add_address(params[:address])
-	 end
-      end
-   end
+			post "/:id/address" do
+				center = Center.find(id: params[:id])
+				center.add_address(params[:address])
+			end
+
+			put "/:id" do
+				center = Center.find(id: params[:id])
+				center.update(params[:center])
+			end
+
+			put "/:id/address/:address_id" do
+				center = Center.find(id: params[:id])
+				address = center.addresses.find(id: params[:address_id]).first
+				address.update(params[:address])
+			end
+
+		end
+	end
+
 end
 
--- spec/api/mods/center_spec.rb
+++ spec/api/mods/center_spec.rb
@@ -22,6 +22,33 @@ describe "Center" do
 
       expect(Address.find(id:resp["id"]).center[:name]).to eql "Yogam"
    end
+
+   it "should be able to edit center" do
+      center = Center.create(name: "Yogam", location: "Singapore")
+
+      put "/api/v1/center/#{center.id}", center: {name: "Yogam Center"}
+
+      resp = JSON.parse(last_response.body)
+
+      expect(resp["name"]).to eql("Yogam Center")
+   end
+
+   it "should be able to edit center address" do
+
+      center = Center.create(name: "Yogam", location: "Singapore")
+      post "/api/v1/center/#{center.id}/address", address: {address:"11 Street", city:"Singapore", country: "Singapore"}
+
+      address = JSON.parse(last_response.body)
+
+      put "/api/v1/center/#{center.id}/address/#{address['id']}", address: {address: "411 Race course Road"}
+
+      response = JSON.parse(last_response.body)
+
+      expect(response["address"]).to eql "411 Race course Road"
+      expect(response["city"]).to eql "Singapore"
+
+      expect(Address.find(id:response["id"]).center[:name]).to eql "Yogam"
+   end
       
 end
 
