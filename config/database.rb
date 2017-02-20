530b2e39cfb6f6615dc8c1e116b90062555eb4ef
Saravana <sgsaravana@gmail.com>
2017-02-20 13:17:47 +0530

    jQuery.min.js moved to public/dist

--- config/database.rb
+++ config/database.rb
@@ -2,8 +2,8 @@ Sequel::Model.plugin(:schema)
 Sequel::Model.plugin(:json_serializer)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect("mysql2://root:@127.0.0.1/venuebook_development", :loggers => [logger])
-  when :production  then Sequel.connect("mysql2://root:1122334Meonly@127.0.0.1/venuebook_production",  :loggers => [logger])
-  when :test        then Sequel.connect("mysql2://root:@127.0.0.1/venuebook_test",        :loggers => [logger])
+  when :development then Sequel.connect("mysql2://root:@127.0.0.1/venuebook_development", :loggers => [logger], :encoding => "utf8")
+  when :production  then Sequel.connect("mysql2://root:1122334Meonly@127.0.0.1/venuebook_production",  :loggers => [logger], :encoding => "utf8")
+  when :test        then Sequel.connect("mysql2://root:@127.0.0.1/venuebook_test",        :loggers => [logger], :encoding => "utf8")
 end
 Sequel::Model.db.extension(:pagination)
