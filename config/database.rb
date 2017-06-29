bad9424631995862643857b205d15254dd54a706
Saravana <sgsaravana@gmail.com>
2017-06-29 18:01:37 +0530

    staging database added

--- config/database.rb
+++ config/database.rb
@@ -4,6 +4,7 @@ Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect("mysql2://root:@127.0.0.1/venuebook_development", :loggers => [logger], :encoding => "utf8")
   when :production  then Sequel.connect("mysql2://root:1122334Meonly@127.0.0.1/venuebook_production",  :loggers => [logger], :encoding => "utf8", :max_connections => 100)
+  when :staging		then Sequel.connect("mysql2://root:1122334Meonly@127.0.0.1/venuebook_staging",  :loggers => [logger], :encoding => "utf8", :max_connections => 100)
   when :test        then Sequel.connect("mysql2://root:@127.0.0.1/venuebook_test",        :loggers => [logger], :encoding => "utf8")
 end
 Sequel::Model.db.extension(:pagination)
