5f0aff731a2926bad29678f54685aac30ec34f2f
Saravana <sgsaravana@gmail.com>
2017-08-01 23:42:58 +0800

    Production branch updated

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -25,7 +25,7 @@ role :app, %w{globaltof.nithyananda.org}
 role :web, %w{globaltof.nithyananda.org}
 role :db,  %w{globaltof.nithyananda.org}
 
-set :branch, 'saravana-dev'
+set :branch, 'release_1.0.0-beta'
 # set :branch, 'master'
 set :deploy_to, '/var/www/venuebook'
 set :rails_env, :production
