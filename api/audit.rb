9f7c3b6e207b2ff9422044922ad9b4aacdc7f288
Saravana <sgsaravana@gmail.com>
2017-09-28 13:33:11 +0800

    Post params done for Audit Logs

--- api/audit.rb
+++ api/audit.rb
@@ -15,22 +15,24 @@ class Audits < Grape::Middleware::Base
 			log[:status]              = status
 			log[:ip]                  = env['REMOTE_ADDR']
 			log[:query_params]        = env['rack.request.query_hash']
-			log[:namespace]           = env['namespace']
+			log[:namespace]           = env["grape.routing_args"][:route_info].options[:namespace]
 			log[:raw_request_body]    = env['api.request.input']
 			log[:parsed_request_body] = env['api.request.body']
 
 			response_body = ""
-
-			current_user = Audits.current_user(env['HTTP_TOKEN'])
-			log[:current_user] = current_user ? current_user[0] : nil
-
 			# read the JSON string line by line
 			response.each { |part| response_body += part }
 			log[:response] = response_body
 
-			puts "\n\n\n========================================="
-			puts log.inspect
-			puts "=========================================\n\n\n"
+			current_user       = Audits.current_user(env['HTTP_TOKEN'])
+			log[:current_user] = current_user ? current_user[0] : nil
+			log[:category]     = Audits.get_category(log[:namespace])
+			log[:sub_category] = Audits.get_sub_category(log[:url])
+			log[:resource]     = Audits.get_resource(log[:namespace], log[:method], log[:url], response_body)
+
+			# puts "\n\n\n========================================="
+			# puts log.inspect
+			# puts "=========================================\n\n\n"
 
 			Audits.log_request(log)
 		end
@@ -58,12 +60,87 @@ class Audits < Grape::Middleware::Base
 		end
 	end
 
+	def self.get_category(namespace)
+		return !namespace.empty? ? namespace.gsub("/","").split("_").map {|a| a.capitalize}.join(" ") : nil
+	end
 
-	def self.log_request(log)
+	def self.get_sub_category(url)
+		arr = url.split('/')
+		if arr.length > 4
+			main_cat = Audits.get_category("/#{arr[2]}")
+			sub_cat  = Audits.get_category("/#{arr[4]}")
+			return "#{main_cat} #{sub_cat}"
+		else
+			return nil
+		end
+	end
+
+	def self.get_resource(namespace, method, url, response)
+		resource = {}
+		resource[:resource_name] = Audits.get_resource_name(namespace)
+		resource[:resource_id]   = resource[:resource_name] ? Audits.get_resource_id(method, url, response) : nil
+		return resource
+	end
+
+	def self.get_resource_name(namespace)
+		case namespace
+		when "/sessions"
+			return nil
+		when "/participants"
+			return "Participant"
+		when "/centers"
+			return "Center"
+		when "/events"
+			return "Event"
+		when "/settings"
+			return "Setting"
+		when "/programs"
+			return "Program"
+		when "/event_attendances"
+			return "EventAttendance"
+		when "/users"
+			return "User"
+		when "/venues"
+			return "Venue"
+		else
+			return ""
+		end
+	end
+
+	def self.get_resource_id(method, url, response)
+		resource_id = nil
+
+		case method
+		when "GET"
+			if url.split('/').length > 3
+				resource_id = url.split('/')[3]
+			end
+			return resource_id
+		when "POST"
+			if url.split('/').length > 3
+				resource_id = url.split('/')[3]
+			end
+			return resource_id
+		when "PUT"
+			resource_id = url.split('/')[3]
+			return resource_id
+		when "DELETE"
+			resource_id = url.split('/')[3]
+			return resource_id
+		else
+			return resource_id
+		end
+	end
+
+	def self.log_request(params)
+		log = Logger.new(STDOUT)
 		begin
-			#
+			# Post data to the AuditLogs API
+			response = RestClient.post AUDITBOOK_URL, params
+			log.info response.inspect
+
 		rescue Exception => e
-			#
+			log.error(e.inspect)
 		end
 	end
 
