52573ab9b3d900c58f86151568785235ce2b992d
Saravana <sgsaravana@gmail.com>
2018-02-24 15:51:22 +0800

    Staging release set branch name

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -25,7 +25,7 @@ role :app, %w{iamsaravana.com}
 role :web, %w{iamsaravana.com}
 role :db,  %w{iamsaravana.com}
 
-set :branch, 'staging'
+set :branch, 'release_1.1.0'
 # set :branch, 'master'
 set :deploy_to, '/var/www/venuebook_staging'
 set :rails_env, :staging
